a7edc14a4b092b3da9e48a531dfcfb19f880ddd2
KimInSoo0119 <70687449+KimInSoo0119@users.noreply.github.com>
2025-05-08 20:35:55 +0900

    [level 1] Title: 문자열 나누기, Time: 0.98 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int solution(String s) {
+      int answer = 0;      
+      int cnt1 = 0;
+      int cnt2 = 0;
+      char c = s.charAt(0);
+      
+      for (int i = 0; i < s.length(); i++) {
+          if (cnt1 == cnt2) {
+              answer++;
+              c = s.charAt(i);
+          }
+          
+          if (c == s.charAt(i)) {
+              cnt1++;
+          } else {
+              cnt2++;
+          }
+      }
+      
+      return answer;
+  }
+}
