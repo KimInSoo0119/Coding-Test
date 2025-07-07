de465c685965716ca092d4c1aae06ac46e18dff4
KimInSoo0119 <70687449+KimInSoo0119@users.noreply.github.com>
2025-07-07 19:53:17 +0900

    [Silver I] Title: 용돈 관리, Time: 232 ms, Memory: 22360 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+
+        arr = new int[N];
+
+        int end = 0;
+        int max = 0;
+        int start = 0;
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            end += arr[i];
+            max = Math.max(arr[i], max);
+        }
+
+        start = max;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if (M >= getMid(mid)) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static int getMid(int tempMoney) {
+        int count = 1;
+        int money = tempMoney;
+
+        for (int num : arr) {
+            money -= num;
+
+            if (money < 0) {
+                count++;
+                money = tempMoney - num;
+            }
+        }
+        return count;
+    }
+ }
